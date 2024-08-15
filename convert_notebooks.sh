79cc7e32c47f5297ca3e8939c0b3ca380325bae9
Teaghan O'Briain <obriaintb@gmail.com>
2024-08-15 19:47:58 +0000

    General file update.

--- convert_notebooks.sh
+++ convert_notebooks.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Remove old output directories if they exist
 rm -r html_lessons/
+rm -r md_files/
+
+# Create new output directories
 mkdir html_lessons/
+mkdir md_files/
 
 # Loop through each Unit directory
 for unit_dir in Unit*; do
@@ -9,8 +14,19 @@ for unit_dir in Unit*; do
   if [ -d "$unit_dir" ]; then
     # Find all .ipynb files in the directory, excluding *-checkpoint.ipynb files, and convert them to HTML
     find "$unit_dir" -name "*.ipynb" ! -name "*-checkpoint.ipynb" -exec jupyter nbconvert --to html {} --output-dir="./html_lessons/" \;
+
+    # Convert the same notebooks to markdown using the custom preprocessor to remove empty cells
+    notebook_files=$(find "$unit_dir" -name "*.ipynb" ! -name "*-checkpoint.ipynb")
+    if [ ! -z "$notebook_files" ]; then
+      python nb_to_md.py $notebook_files "./md_files/"
+    fi
+    
+    # Run the Python script to fix HTML files (if needed)
     python fix_html.py "./html_lessons/"
   fi
 done
 
-echo "HTML files created successfully."
+python fix_md.py
+python combine_content.py 
+
+echo "HTML and Markdown files created successfully."
--- update_git.sh
+++ update_git.sh
@@ -8,13 +8,29 @@ for unit in "${units[@]}"; do
   find "${unit}" -type f \( -name "*.ipynb" -o -name "*.html" \) ! -name "*-checkpoint.ipynb" ! -path "*/.ipynb_checkpoints/*" -exec git add {} \;
   git add "${unit}/figures/*"
   git add "${unit}/README.md"
+  git add "${unit}/*.docx"
+  git add "${unit}/*.pdf"
 done
 
 git add "images/*"
 git add "html_lessons/*"
+git add "md_files/*"
+git add "_layouts/*"
+git add "_sass/*"
+git add "_includes/*"
+git add "assets/*"
 git add "Assistant_Instructions.txt"
+git add "Tutor_Instructions.txt"
 git add "bc_science_curriculum.txt"
 git add "README.md"
+git add "_config.yml"
+git add "index.md"
+git add "fix_md.py"
+git add "nb_to_md.py"
+git add "combine_content.py"
+git add "course_content.txt"
+git add "convert_notebooks.sh"
+git add "update_git.sh"
 
 # Commit the changes with a meaningful commit message
 git commit -m "General file update."
